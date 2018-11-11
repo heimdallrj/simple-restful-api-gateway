6f958857bfc525ff1af055f9cf15384c6836b0fa
thinkholic <mailtokasun@gmail.com>
2018-11-11 12:46:35 +0530

    Update configs.

---
+++ config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+require("dotenv").config();
+
+const config = {
+  PORT: process.env.PORT,
+  SECRET_KEY: process.env.SECRET_KEY,
+  DB_DRIVER: process.env.DB_DRIVER,
+  DB_CONNECTIONS: [
+    {
+      driver: process.env.DB_DRIVER,
+      url: process.env.MONGO_URL
+    }
+  ],
+  API_CONFIG_PATH: path.join(__dirname, "api.json"),
+  HANDLER_PATH: path.join(__dirname, "handlers", "http"),
+  MIDDLEWARE_PATH: path.join(__dirname, "middlewares")
+};
+
+module.exports = config;
